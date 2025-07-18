a6a569cfe390d15bd827a179d9b2bc830c135d81
Ashadowmonarch <forcoding39@gmail.com>
2025-07-18 08:55:27 -0400

    create data format

---
+++ generate_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from faker import Faker # For generating test data
+import random
+import time
+
+fake = Faker()
+num_records = 5_000_000 # Number convention for easier interpretation
+
+print(f"Generating {num_records} records...")
+start_time = time.time()
+
+data = {
+    'transaction_id': [fake.uuid4() for _ in range(num_records)],
+    'product_id': [random.randint(1, 1000) for _ in range(num_records)],
+    'customer_id': [random.randint(1,10000) for _ in range(num_records)],
+    'store_id': [random.randint(1, 100) for _ in range(num_records)],
+    'transaction_date': [fake.data_time_this_decade() for _ in range(num_records)],
+    'quantity': [random.randint(1,5) for _ in range(num_records)],
+    'unit_price': [round(random.uniform(5.0, 500.0), 2) for _ in range(num_records)],
+    'discount': [round(random.uniform(0.0, 0.25), 2) for _ in range(num_records)]
+}
