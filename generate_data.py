e2b41229a35b6826d4ee352fe8e26f624d14b2a2
Ashadowmonarch <forcoding39@gmail.com>
2025-07-18 08:58:51 -0400

    correct method name

--- generate_data.py
+++ generate_data.py
@@ -14,7 +14,7 @@ data = {
     'product_id': [random.randint(1, 1000) for _ in range(num_records)],
     'customer_id': [random.randint(1,10000) for _ in range(num_records)],
     'store_id': [random.randint(1, 100) for _ in range(num_records)],
-    'transaction_date': [fake.data_time_this_decade() for _ in range(num_records)],
+    'transaction_date': [fake.date_time_this_decade() for _ in range(num_records)],
     'quantity': [random.randint(1,5) for _ in range(num_records)],
     'unit_price': [round(random.uniform(5.0, 500.0), 2) for _ in range(num_records)],
     'discount': [round(random.uniform(0.0, 0.25), 2) for _ in range(num_records)]
